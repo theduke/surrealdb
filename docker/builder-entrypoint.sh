3e75d94b3e853c7edfc48fff08f35e1c691b0870
Salvador Girones <salvadorgirones@gmail.com>
2024-01-17 10:38:52 +0100

    fix build

--- docker/builder-entrypoint.sh
+++ docker/builder-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 . /opt/rh/gcc-toolset-13/enable
+
+ln -s /opt/rh/gcc-toolset-13/root/usr/bin/aarch64-redhat-linux-gcc /opt/rh/gcc-toolset-13/root/usr/bin/aarch64-linux-gnu-gcc
+
 exec cargo build "$@"

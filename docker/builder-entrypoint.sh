6535c3ff2678ea9ea6377bd2deefb8ba4fbb5ec0
Salvador Girones <salvadorgirones@gmail.com>
2024-01-17 20:36:46 +0100

    move config

--- docker/builder-entrypoint.sh
+++ docker/builder-entrypoint.sh
@@ -2,6 +2,4 @@
 
 . /opt/rh/gcc-toolset-13/enable
 
-ln -s /opt/rh/gcc-toolset-13/root/usr/bin/aarch64-redhat-linux-gcc /opt/rh/gcc-toolset-13/root/usr/bin/aarch64-linux-gnu-gcc
-
 exec cargo build "$@"
